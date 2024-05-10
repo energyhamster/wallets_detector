683f5b87439068aad783ddc8335d285d4701bd64
Vladyslav Opanasenko <vla.opanasenko@gmail.com>
2024-05-10 13:16:32 +0300

    test running by cron 10

--- conftest.py
+++ conftest.py
@@ -1,12 +1,13 @@
 import pytest
 import undetected_chromedriver as uc
-
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture()
 def driver():
+    options = Options()
     driver = uc.Chrome()
     driver.implicitly_wait(160)
-    driver.maximize_window()
+    options.add_argument("--window-size=1920,1080")
     try:
         yield driver
     finally:
